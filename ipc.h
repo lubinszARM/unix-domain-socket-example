32d9dcb7cf3bc4b37d4bd270c6efb881c02457d2
Robin Luk <lubinsz@hotmail.com>
2021-02-19 03:15:17 +0000

    add absolute path for sock file
    Signed-off-by: Robin Luk <lubinsz@hotmail.com>

--- ipc.h
+++ ipc.h
@@ -1,7 +1,7 @@
 #ifndef IPC_H
 #define IPC_H
 
-#define CLIENT_SOCK_FILE "client.sock"
-#define SERVER_SOCK_FILE "server.sock"
+#define CLIENT_SOCK_FILE "/tmp/client.sock"
+#define SERVER_SOCK_FILE "/tmp/server.sock"
 
 #endif
